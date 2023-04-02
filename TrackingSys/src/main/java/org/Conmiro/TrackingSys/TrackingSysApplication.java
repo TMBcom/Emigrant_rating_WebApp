4397a98676715c9ba4026b92cbd0f0f6a4275aa4
CONMIROPC\redli <tmb.command@gmail.com>
2023-04-03 00:26:47 +0300

    InitCommit

---
+++ TrackingSys/src/main/java/org/Conmiro/TrackingSys/TrackingSysApplication.java
@@ -0,0 +1,13 @@
+package org.Conmiro.TrackingSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingSysApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingSysApplication.class, args);
+
+	}
+
+}
---
+++ TrackingSys/src/test/java/org/Conmiro/TrackingSys/TrackingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Conmiro.TrackingSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
