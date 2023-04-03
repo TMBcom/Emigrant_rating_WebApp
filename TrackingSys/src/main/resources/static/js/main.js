3c6a8dc2910778247044a0b3520615e9ab1dbfe8
CONMIROPC\redli <tmb.command@gmail.com>
2023-04-03 13:52:23 +0300

    feat:view controller DB output

--- TrackingSys/src/main/resources/static/js/main.js
+++ TrackingSys/src/main/resources/static/js/main.js
@@ -1,20 +1,31 @@
+var messageApi = Vue.resource('/message{/id}')
 
 
+Vue.component('message-row', {
+props: ['message'],
+    template: '<div><i>({{message.id}})</i> {{ message.text }}</div>'
+});
 
 
 Vue.component('messages-list',{
 props: ['messages'],
-template:'<div><div v-for="message in messages">{{ message.text }}</div></div>'
+template:
+'<div>' +
+'<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+'</div>',
+created: function(){
+messageApi.get().then(result =>
+    result.json().then( data =>
+    data.forEach(message => this.messages.push(message))
+    )
+)
+}
 });
 
 var app = new Vue({
   el: '#app',
   template: '<messages-list :messages="messages"/>',
   data: {
-    messages:[
-    { id:'123', text: 'Wow'},
-    { id:'13', text: 'Meen'},
-    { id:'3', text: 'Sometext'},
-    ]
+    messages:[]
   }
 });
