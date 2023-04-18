cf8789a9006cf24fac2a0b012b00bf6c4bcd02c1
CONMIROPC\redli <tmb.command@gmail.com>
2023-04-19 00:33:22 +0300

    feat: freemarker

--- TrackingSys/src/main/java/org/Conmiro/TrackingSys/Controllers/ProductController.java
+++ TrackingSys/src/main/java/org/Conmiro/TrackingSys/Controllers/ProductController.java
@@ -1,4 +1,13 @@
 package org.Conmiro.TrackingSys.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+    @GetMapping("/product")
+    public String product() {
+        return "products";
+    }
+
 }
