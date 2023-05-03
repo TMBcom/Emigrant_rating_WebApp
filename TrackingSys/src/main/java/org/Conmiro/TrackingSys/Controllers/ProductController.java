da53570db955590f0e52460706e0957630ae9800
CONMIROPC\redli <tmb.command@gmail.com>
2023-05-03 22:21:23 +0300

    feat:product-info.ftlh

--- TrackingSys/src/main/java/org/Conmiro/TrackingSys/Controllers/ProductController.java
+++ TrackingSys/src/main/java/org/Conmiro/TrackingSys/Controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.Conmiro.TrackingSys.Service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,15 +19,20 @@ public class ProductController {
         model.addAttribute("products", productService.listProducts());
         return "products";
     }
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable long id, Model model) {
+        return "product-info";
+    }
     @PostMapping("/product/create")
     public String createProduct(ProductModel product) {
         productService.saveProduct(product);
         return "redirect:/";
     }
 
-    
-    public String deleteProduct(long id){
-
+    @PostMapping("product/delete/{id}")
+    public String deleteProduct(@PathVariable long id){
+        productService.deleteProducts(id);
+        return "redirect:/";
     }
 
 }
